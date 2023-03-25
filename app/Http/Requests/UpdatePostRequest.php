60157bec58183b62d190d7657127206f1ea26aa2
africam <africamatji@gmail.com>
2023-03-25 23:16:16 +0200

    update post request class for validation

--- app/Http/Requests/UpdatePostRequest.php
+++ app/Http/Requests/UpdatePostRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreatePostRequest extends FormRequest
+class UpdatePostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,9 @@ class CreatePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:255',
-            'body' => 'required|string|max:255',
-            'category_id' => 'required|integer'
+            'title' => 'string|max:255',
+            'body' => 'string|max:255',
+            'category_id' => 'integer'
         ];
     }
 }
